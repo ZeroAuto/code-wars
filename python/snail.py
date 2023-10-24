c15aafe94a9ef03736a4345f471c6b0dc93a552c
Mike James <mikejames386@gmail.com>
2023-10-24 04:03:57 -0500

    better snail solution

--- python/snail.py
+++ python/snail.py
@@ -1,47 +1,26 @@
 def snail(arr):
-    total = len(arr) * len(arr[0])
-    count = 0
     result = []
-    current_direction = 'left'
+    while len(arr) > 0:
+        # right first
+        result += arr[0]
+        del arr[0]
 
-    while count < total:
-        if current_direction == 'left':
-            sub_count = 0
-            for val in arr[0]:
-                result.append(val)
-                sub_count += 1
-            arr.pop(0)
-            count += sub_count
-            current_direction = 'down'
-        if current_direction == 'down':
-            sub_count = 0
+        if len(arr) > 0:
+            # down
             for i in arr:
-                idx = len(i) - 1
-                result.append(i[idx])
-                i.pop(idx)
-                print(arr)
-                sub_count += 1
-            count += sub_count
-            current_direction = 'right'
-        if current_direction == 'right':
-            for i in list(reversed(arr[len(arr) - 1])):
-                result.append(i)
-                sub_count += 1
-            arr.pop(len(arr) - 1)
-            count += sub_count
-            current_direction = 'up'
-        if current_direction == 'up':
-            sub_count = 0
-            for i in reversed(arr):
-                result.append(i[0])
-                i.pop(0)
-                print(arr)
-                sub_count += 1
-            count += sub_count
-            current_direction = 'left'
+                result += [i[-1]]
+                del i[-1]
 
-    if len(arr) > 0:
-        result.append(arr[0][0])
+            # left
+            if arr[-1]:
+                result += arr[-1][::-1]
+                del arr[-1]
+
+            # up
+            for i in reversed(arr):
+                result += [i[0]]
+                del i[0]
+
     return result
 
 
