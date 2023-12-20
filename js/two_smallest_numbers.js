7f8b448585b1b636bc5ddfc215234d7994071688
Mike James <mikejames386@gmail.com>
2023-12-20 13:54:01 -0600

    update two smallest number solution

--- js/two_smallest_numbers.js
+++ js/two_smallest_numbers.js
@@ -1,8 +1,9 @@
 const sumTwoSmallestNumbers = (numbers) => {
-  // do something
-  console.log('do this thing');
-}
+  numbers.sort((a,b) => a - b);
+  return numbers[0] + numbers[1]
+};
 
-sumTwoSmallestNumbers('blah');
+const sumTwoSmallestNumbersReduce = numbers => numbers.sort((a,b) => a - b).slice(0,2).reduce((a,b) => a+b);
 
-console.log('did anything happen');
+console.log(sumTwoSmallestNumbersReduce([5,8,12,19,22])) // 13
+console.log(sumTwoSmallestNumbersReduce([15,28,4,2,43])) //6
