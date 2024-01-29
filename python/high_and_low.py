04f2700f418402d5e16ba56bb338c4f427c5f823
Mike James <mikejames386@gmail.com>
2024-01-29 16:18:48 -0600

    add high and low solution

---
+++ python/high_and_low.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/554b4ac871d6813a03000035/train/python
+def high_and_low(str):
+    arr = sorted([int(n) for n in str.split(" ")])
+    return f"{arr[len(arr) - 1]} {arr[0]}"
+
+
+# I always forget about min and max
+def high_and_low_min_max(str):
+    arr = sorted([int(n) for n in str.split(" ")])
+    return f"{max(arr)} {min(arr)}"
+
+
+print(high_and_low_min_max("4 2 3 1 6"))
