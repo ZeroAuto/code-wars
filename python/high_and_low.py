5a3978137aea6cb22593adeb55bfd9a2ec2c5977
Mike James <mikejames386@gmail.com>
2024-01-29 16:22:48 -0600

    derp

--- python/high_and_low.py
+++ python/high_and_low.py
@@ -1,7 +1,7 @@
 # https://www.codewars.com/kata/554b4ac871d6813a03000035/train/python
 def high_and_low(str):
     arr = sorted([int(n) for n in str.split(" ")])
-    return f"{arr[len(arr) - 1]} {arr[0]}"
+    return f"{arr[-1]} {arr[0]}"
 
 
 # I always forget about min and max
