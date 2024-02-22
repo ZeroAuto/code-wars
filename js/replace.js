e9c60dc1b8d2e9edd925b905ede89023c2fc775b
Mike James <mikejames386@gmail.com>
2024-02-21 18:41:44 -0600

    a few more javascript solutions

---
+++ js/replace.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed/train/javascript
+const replace = s => s.replace(/[aeiouAEIOU]/g, '!');
+
+const replaceI = s => s.replace(/[aeiou]/ig, '!');
---
+++ js/unusual_five.js
@@ -0,0 +1,2 @@
+// https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+const unusualFive = () => 'aaaaa'.length;
