76ebd236eebc0d8452bbc1d6c79240fcf059a49c
mike <mike@mikes-MacBook-Air.local>
2024-08-27 10:39:25 -0400

    add a couple of more javascript exercises

---
+++ js/capitalize.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/59cfc000aeb2844d16000075
+
+const capitalize = s => {
+  const odd = s.split("").map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join("");
+  const even = s.split("").map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join("");
+  return [even, odd];
+}
